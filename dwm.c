10631e453c6c7690ca95f6f69dedf7042f2d0690
Diego Cirilo <dvcirilo@gmail.com>
2025-01-06 04:36:05 -0300

    adding smartgaps

--- dwm.c
+++ dwm.c
@@ -1300,7 +1300,7 @@ resizeclient(Client *c, int x, int y, int w, int h)
 		gapincr = gapoffset = 0;
 	} else {
 		/* Remove border and gap if layout is monocle or only one client */
-		if (c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) {
+		if ((c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) && smartgaps == 1) {
 			gapoffset = 0;
 			gapincr = -2 * borderpx;
 			wc.border_width = 0;
