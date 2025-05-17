adcfe301bcbb5433fd4091273d5d9f07284f93a9
Diego Cirilo <dvcirilo@gmail.com>
2025-05-17 12:47:08 -0300

    config

--- config.def.h
+++ config.def.h
@@ -1,21 +1,22 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 10;       /* gaps between windows */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int maxsww    = 1920;     /* emulate 16:9 on ultrawide when singlewindow */
-static const int rmaster            = 1;        /* 1 means master-area is initially on the right */
 static const int showbar            = 1;        /* 0 means no bar */
+static const int rmaster            = 0;        /* 1 means master-area is initially on the right */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Noto Mono:size=11:antialias=true:autohint=true", "Noto Emoji Medium:size=11:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Noto Mono:size=11:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#005070";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -30,20 +31,23 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	/* class                    instance    title       tags mask    iscentered   isfloating   monitor */
+	{ "Lingot",                 NULL,       NULL,          0,          0,               1,           -1 },
+	{ "Pianoteq",               NULL,       NULL,          0,          0,               1,           -1 },
+	{ "newRPL",                 NULL,       NULL,          0,          0,               1,           -1 },
+	{ "DB48X",                  NULL,       NULL,          0,          0,               1,           -1 },
+	{ "SimpleScreenRecorder",   NULL,       NULL,          0,          1,               1,           -1 }
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const int lpm[] = {
         /* Index of preferred layout], if LENGTH(lpm)<#monitors -> default layout */
-        0, 1
+        0, 0
 };
 
 static const Layout layouts[] = {
@@ -72,6 +76,18 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *termsamedir[]  = { "samedir", NULL };
+static const char *upvol[]            = { "pulseaudio-ctl", "up" };
+static const char *downvol[]          = { "pulseaudio-ctl", "down" };
+static const char *mutevol[]          = { "pulseaudio-ctl", "mute" };
+static const char *mutemic[]          = { "pulseaudio-ctl", "mute-input" };
+static const char *backlightup[]      = { "xbacklight", "-inc", "10" };
+static const char *backlightdown[]    = { "xbacklight", "-dec"," 10" };
+static const char *brightup[]         = { "brightness", "-inc" };
+static const char *brightdown[]       = { "brightness", "-dec" };
+static const char *playpause[]        = { "playerctl", "play-pause", NULL };
+static const char *play_next[]        = { "playerctl", "next", NULL };
+static const char *play_prev[]        = { "playerctl", "previous", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -80,22 +96,22 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_minus,  incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_g,      goback,         {0} },
+	{ MODKEY,                       XK_space,  zoom,           {0} },
+	{ MODKEY,                       XK_s,      view,           {0} },
+	{ MODKEY|ShiftMask,             XK_s,      goback,         {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_d,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_v,      setlayout,      {.v = &layouts[6]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_Return, setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_r,      togglermaster,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -104,7 +120,23 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
+    { 0,                            XK_Print,  spawn,          SHCMD("maim -s -u | xclip -selection clipboard -t image/png") },
+    { ShiftMask,			        XK_Print,  spawn,          SHCMD("maimpick") },
+	{ MODKEY,                       XK_o,      spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_o,      spawn,          {.v = termsamedir } },
+	{ Mod4Mask,                     XK_l,      spawn,          SHCMD("slock") },
+	{ 0,                            XF86XK_AudioRaiseVolume,   spawn, {.v = upvol } },  
+	{ 0,                            XF86XK_AudioLowerVolume,   spawn, {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute,          spawn, {.v = mutevol } },
+	{ 0,                            XF86XK_AudioMicMute,       spawn, {.v = mutemic } },
+	{ 0,                            XF86XK_MonBrightnessUp,    spawn, {.v = backlightup } },
+	{ 0,                            XF86XK_MonBrightnessDown,  spawn, {.v = backlightdown } },
+    { 0,					        XF86XK_AudioPlay,          spawn, {.v = playpause } },
+	{ 0,					        XF86XK_AudioPrev,          spawn, {.v = play_prev } },
+	{ 0,					        XF86XK_AudioNext,          spawn, {.v = play_next } },
+	{ Mod4Mask,                     XK_k,       spawn,         {.v = brightup } },
+	{ Mod4Mask,                     XK_j,       spawn,         {.v = brightdown } },
+    TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
@@ -132,4 +164,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
